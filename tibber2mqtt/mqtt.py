c35a0cd6c42c6356a8cc14545892b4eefb931a8b
Daniel Ring <danielring.ch@googlemail.com>
2024-01-14 12:15:16 +0100

    added support for auth and TLS

--- tibber2mqtt/mqtt.py
+++ tibber2mqtt/mqtt.py
@@ -1,5 +1,6 @@
 import struct
 import paho.mqtt.client as mqtt
+from ssl import CERT_NONE
 from helpers import get_recursive_key
 from logger import *
 
@@ -11,6 +12,18 @@ class Mqtt():
         self.__mqtt.on_connect = self.__on_connect
 
         ip, port = get_recursive_key(config, 'host').split(':')
+
+        ca = get_recursive_key(config, 'ca', optional=True)
+        public_key = get_recursive_key(config, 'public_key', optional=True)
+        private_key = get_recursive_key(config, 'private_key', optional=True)
+        if ca or public_key or private_key:
+            insecure = get_recursive_key(config, 'tls_insecure', optional=True) == True
+            self.__mqtt.tls_set(ca_certs=ca, certfile=public_key, keyfile=private_key, cert_reqs=CERT_NONE if insecure else None)
+
+        user = get_recursive_key(config, 'user', optional=True)
+        password = get_recursive_key(config, 'password', optional=True)
+        if user or password:
+            self.__mqtt.username_pw_set(user, password)
 
         self.__topic = get_recursive_key(config, 'topic')
 
